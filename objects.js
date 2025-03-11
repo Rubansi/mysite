827978e7cfd9a0a627709da03da982437a7f28d5
Rubansi Vincent <126601445+Rubansi@users.noreply.github.com>
2025-03-11 21:33:05 +0300

    JS Objects

---
+++ objects.js
@@ -0,0 +1,39 @@
+console.log ("Hello Js")
+// objects - datatypes
+
+const thisClass = "Fullstcak web dev"
+const week = 8
+
+// Datatypes - number (integers, floats (decimals)), string, boolean (true/false), object, 
+console.log (typeof week)
+console.log ("10"+"10")
+const isRaining = true//false
+
+let x = 5
+let y = 5
+console.log (x+y)
+console.log (x-y)
+
+
+//An example of an object
+const animal = {
+    legs: 4,
+    head: {
+        eyes: 2,
+        haircolor: "brown"
+    },
+    name:"goat"
+}
+
+//crud
+
+//READ - Accessing object properties - 
+// const name = object.property
+console.log (animal.name)
+console.log (animal.head.eyes)
+
+//Uisng the brackets
+console.log (animal ["name"])
+console.log (animal ["name"],["head"],["eyes"])
+
+
