15de3eb0f580ef7f2a8dd6a1f5099d8c3950bf29
Rubansi Vincent <126601445+Rubansi@users.noreply.github.com>
2025-03-25 20:29:11 +0300

    operators

--- rizwan class/objects.js
+++ rizwan class/objects.js
@@ -48,9 +48,64 @@ console.log (animal)
 delete animal.head.haircolor
 console.log (animal)
 
+<<<<<<< Updated upstream
 console.log
 console.log {
     []
 }
 
 
+=======
+
+//console.log ("Calling")
+//objects can have properties and methods
+// a method is like a function
+//console is an inbuilt object. There are objects already in JS
+// .log ("") is a function being called
+// we will learn about object methods. Nested objects are possible.
+
+//Comparison operartors
+
+// console.log(7 > 7) // false
+// console.log(7 == 7) // true
+// console.log(7 => "7") // false
+// console.log(7 <= "7") // false
+// console.log(7 != "7") // false
+// console.log(7 !=== "7") //
+
+
+// const student = {
+//     name: "Kevin",
+//     math: 60,
+//     eng: 80,
+//     phy: 50,
+// }
+
+const student = {
+    name: "Martin",
+    math: 10,
+    eng: 50,
+    phy: 20,
+}
+//The Total Operator
+const total = student.math + student.eng + student.phy
+console.log(total)
+
+
+
+//Conditionals
+// if
+// else if
+// else
+// switch
+
+if (total >= 150) {
+    console.log ("Yo have passed")
+}else {
+    console.log ("You have failed")
+}
+
+
+
+
+>>>>>>> Stashed changes
